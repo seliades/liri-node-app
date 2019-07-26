e297e6d0cda9857241169f20660559fdfccf3b6d
Stu Eliades <stuart.eliades@gmail.com>
2019-07-26 00:29:15 -0400

    added do-what-it-says function, refined returned results for all functions, added default search queries if none are entered

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# liri-node-app
+# LIRI Node App 
+
+[Link to Video of Working Application]()
+
+## Description
+This LIRI Node application is a Language Interpretation and Recognition Interface that runs through the command line and utlizes JavaScript and several Node modules and APIs. LIRI accepts several commands and returns relavent information to the user based on the command and search query used. If you would like to clone this app you are welcome to, but you will need to supply your own Spotify API credentials in a `.env` file for it to work. On run will prompt for a command and your search, every word after command is the search parameters for the API requests sent out. Each search is logged to the log.txt file. After searching all returned information will then be displayed, and the app will continue to prompt.
+
+## Commands Used & What is Returned
+- "spotify-this-song"/"spotify"/"s": Takes a song name as search query and returns 5 songs that best match. Info displayed is the artist's name, song title, album title, and a link to a 30 second preview of the song via Spotify if available.
+- "concert-this"/"concert"/"c": Takes a band name as search query and returns 5 upcoming concerts for that artist. Info displayed is the artist's name, name and location of the venue (city, region, & country), and the date of the concert. 
+- "movie-this"/"movie"/"m": Takes a movie title as search query and returns most relevant movie result. Info displayed is the movie title, year of release, IMDB and Rotten Tomatoes ratings if available, country the movie was produced in, the langauage spoken, a plot synopsis, and the lead actors.
+- "do-what-it-says"/"random"/"r": Pulls command and search query from `random.txt` file and runs applicable functions. The command and search query can be changed in the file.
+
+## Modules Used
+- node-spotify-api
+- dotenv
+- axios
+- moment
+- inquirer
--- liri.js
+++ liri.js
@@ -20,7 +20,7 @@ function inputLine() {
         let input = result.input.split(" ");
         let command = input[0].toLowerCase();
         let terms = input.slice(1).join(" ").toLowerCase();
-        
+
         if (command === "spotify-this-song" || command === "spotify" || command === "s") {
             spotifyThis(terms);
             log(result.input);
@@ -33,7 +33,7 @@ function inputLine() {
             movieThis(terms);
             log(result.input);
         }
-        else if (command === "do-what-it-says" || command === "random" || command === "dwis" || command === "r") {
+        else if (command === "do-what-it-says" || command === "random" || command === "r") {
             randomThis();
         }
         else {
@@ -55,43 +55,51 @@ function inputLine() {
 
 
 function spotifyThis(terms) {
+    if (!terms) {
+        terms = "The Sign";
+    }
     spotify.search({ type: 'track', query: terms, limit: 5 })
-    .then (function(response) {
-        let tracks = response.tracks;
-        for (let i = 0; i < tracks.items.length; i++) {
-        console.log("\n");
-        console.log("~~~~~~~~~~~~~~~ SONG INFO ~~~~~~~~~~~~~~~");
-        console.log("Artist name: " + tracks.items[i].artists[0].name);
-        console.log("Song title: " + tracks.items[i].name);
-        console.log("Album name: " + tracks.items[i].album.name);
-        console.log("Preview link: " + tracks.items[i].preview_url);
-        }
-    })
-   
-    inputLine();
+        .then(function (response) {
+            let tracks = response.tracks;
+            for (let i = 0; i < tracks.items.length; i++) {
+                console.log("\n");
+                console.log("~~~~~~~~~~~~~~~ SONG INFO ~~~~~~~~~~~~~~~\n");
+                console.log("Artist name: " + tracks.items[i].artists[0].name + "\n");
+                console.log("Song title: " + tracks.items[i].name + "\n");
+                console.log("Album name: " + tracks.items[i].album.name + "\n");
+                console.log("Preview link: " + tracks.items[i].preview_url + "\n");
+            }
+            inputLine();
+        })
 }
 
 function concertThis(terms) {
+    if (!terms) {
+        terms = "Built to Spill";
+    }
     axios.get("https://rest.bandsintown.com/artists/" + terms + "/events?app_id=codingbootcamp")
         .then(function (response) {
             let concerts = response.data;
 
-            for (let i = 0; i < concerts.length; i++) {
+            for (let i = 0; i < 5; i++) {
                 console.log("\n");
                 console.log("~~~~~~~~~~~~~~~ EVENT INFO ~~~~~~~~~~~~~~~\n");
+                console.log("Artist: " + concerts[i].lineup + "\n")
                 console.log("Venue Name: " + concerts[i].venue.name + "\n");
                 console.log("Venure Location: " + concerts[i].venue.city + ", " + concerts[i].venue.region + ", " + concerts[i].venue.country + "\n");
-                console.log("~~~~~~~~~~~~~~~ EVENT INFO ~~~~~~~~~~~~~~~\n");
-                console.log("\n");
+                console.log("Date: " + concerts[i].datetime);
             }
+            inputLine();
         })
         .catch(function (error) {
             console.log(error);
         })
-    inputLine();
 }
 
 function movieThis(terms) {
+    if (!terms) {
+        terms = "Mr. Nobody";
+    }
     axios.get("http://www.omdbapi.com/?apikey=812d4567&t=" + terms)
         .then(function (response) {
             let info = response.data;
@@ -104,11 +112,36 @@ function movieThis(terms) {
             console.log("Produced in: " + info.Country + "\n");
             console.log("Language: " + info.Language + "\n");
             console.log("Plot Synopsis: " + info.Plot + "\n");
-            console.log("Cast: " + info.Actors + "\n");
+            console.log("Cast: " + info.Actors);
+            inputLine();
         })
         .catch(function (error) {
             console.log(error);
         })
-    inputLine();
+
 }
-// function randomThis()
+
+function randomThis() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+
+        if (error) {
+            return console.log(error);
+        }
+
+        let input = data.split(",");
+        let command = input[0].toLowerCase();
+        let terms = input.slice(1).join(" ").toLowerCase();
+
+        if (command === "spotify-this-song" || command === "spotify" || command === "s") {
+            spotifyThis(terms);
+        }
+        else if (command === "concert-this" || command === "concert" || command === "c") {
+            concertThis(terms);
+        }
+        else if (command === "movie-this" || command === "movie" || command === "m") {
+            movieThis(terms);
+        }
+
+        inputLine();
+    });
+}
